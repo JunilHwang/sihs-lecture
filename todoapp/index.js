3739644f55c22da34ecef7cb936758d410a7e95c
junil-hwang <tjsdlf4261@naver.com>
2019-02-13 03:16:26 +0900

    todoapp 완성

---
+++ todoapp/index.js
@@ -0,0 +1,95 @@
+// 외부 모듈 import
+const express = require('express')
+const db = require('./data/db.js')
+const app = express()
+
+// middle ware 등록
+app.use(express.json())  // request body를 사용하기 위함
+app.use(express.static('public')) // static file을 사용하기 위함
+
+
+// index page
+app.get('/', (req, res) => {
+  res.render('index') // index.html render
+})
+
+// folder
+app.route('/api/folder')
+  .get(async (req, res) => {
+    const result = {success: true}
+    try {
+      const json = await db.getData()
+      result.data = json.folder
+    } catch (err) {
+      result.success = false
+      result.err = err
+    }
+    res.json(result)
+  })
+  .post(async (req, res) => {
+    const result = {success: true}
+    const folder = req.body.folder
+    try {
+      const json = await db.getData()
+      json.folder = folder
+      await db.setData(json)
+    } catch (err) {
+      result.success = false
+      result.err = err
+    }
+    res.json(result)
+  })
+
+// task
+app.route('/api/task/:parent')
+  .get(async (req, res) => {
+    const result = {success: true}
+    const parent = req.params.parent
+    try {
+      const json = await db.getData()
+      list = []
+      json.task.forEach((v, idx) => {
+        if (v.parent === parent) {
+          v.idx = idx
+          list.push(v)
+        }
+      })
+      result.data = list
+    } catch (err) {
+      result.success = false
+      result.err = err
+    }
+    res.json(result)
+  })
+  .post(async (req, res) => {
+    const result = {success: true}
+    const task = req.body.task
+    const parent = req.params.parent
+    try {
+      const json = await db.getData()
+      task.parent = parent
+      json.task.push(task)
+      await db.setData(json)
+    } catch (err) {
+      result.success = false
+      result.err = err
+    }
+    res.json(result)
+  })
+  .put(async (req, res) => {
+    const result = {success: true}
+    const task = req.body.task
+    const idx = req.params.parent
+    try {
+      const json = await db.getData()
+      json.task[idx] = task
+      await db.setData(json)
+    } catch (err) {
+      result.success = false
+      result.err = err
+    }
+    res.json(result)
+  })
+
+
+app.listen(3000)
--- todoapp/public/js/app.js
+++ todoapp/public/js/app.js
@@ -7,9 +7,7 @@ const create = (name, attr) => {
     const v = attr[k]
     switch (k) {
       case 'html' : ele.innerHTML = v; break
-      case 'event' :
-        for (const e in v) ele.addEventListener(e, v[e])
-      break;
+      case 'event' : for (const e in v) ele.addEventListener(e, v[e]) break;
       default : ele.setAttribute(k, v); break
     }
   }
@@ -20,50 +18,25 @@ const create = (name, attr) => {
 class Ajax {
   static async get (url) {
     const json = await fetch(url).then(res => res.json())
-    if (json.success) {
-      return json.data
-    } else {
-      console.log(json.err)
-      throw `${url} : Ajax GET Error `
-    }
+    if (!json.success) throw json.err
+    return json.data
   }
   static async set (url, data, method = 'post') {
     const headers = { 'Content-Type': 'application/json' }
     const params = { method, headers, body: JSON.stringify(data) }
     const json = await fetch(url, params).then(res => res.json())
-    if (json.success) {
-      return json.data
-    } else {
-      console.log(json.err)
-      throw `${url} : Ajax POST Error `
-    }
+    if (!json.success) throw json.err
+    return json.data
   }
 }
 
 // DataBase
 class Model {
-
-  async getFolder () {
-    return await Ajax.get('/api/folder')
-  }
-
-  async setFolder (folder) {
-    await Ajax.set('/api/folder', { folder })
-  }
-
-  async getTask (parent) {
-    return await Ajax.get('/api/task/' + parent) || []
-  }
-
-  async addTask (task, parent) {
-    await Ajax.set('/api/task/' + parent, { task })
-  }
-
-  async setTask (task, idx) {
-    console.log(idx)
-    await Ajax.set('/api/task/' + idx, { task }, 'put')
-  }
-
+  async getFolder () { return await Ajax.get('/api/folder')}
+  async setFolder (folder) { await Ajax.set('/api/folder', { folder })}
+  async getTask (parent) { return await Ajax.get('/api/task/' + parent) || []}
+  async addTask (task, parent) { await Ajax.set('/api/task/' + parent, { task })}
+  async setTask (task, idx) { await Ajax.set('/api/task/' + idx, { task }, 'put')}
 }
 
 // Renderer
@@ -83,9 +56,9 @@ class Renderer {
         const folder = await $this.model.getFolder()
         folder.push({ name: e.target.value })
         await $this.model.setFolder(folder)
+        $this.folderRender(folder)
         e.target.value = ''
         e.target.focus
-        $this.folderRender(folder)
       }
     }
   }
@@ -97,6 +70,14 @@ class Renderer {
         await $this.model.addTask({name: inputEvent.target.value, state: false}, parent)
         folderEvent.target.click()
       }
+    }
+  }
+
+  setTask (v) {
+    return async e => {
+      v.state = !v.state
+      await $this.model.setTask(v, v.idx)
+      e.target.style.color = v.state ? '#09F' : ''
     }
   }
 
@@ -115,28 +96,20 @@ class Renderer {
       const title = create('h3', {html: folderName})
       const ul = create('ul')
       const task = await $this.model.getTask(parent)
-      const input = create('input', {class: 'task-input', size: 20, placeholder: 'task 입력',
-        event: {
-          keyup: $this.addTask(e, parent)
-        }
-      })
+      const input = create('input', {class: 'task-input', size: 20, placeholder: 'task 입력', event: { keyup: $this.addTask(e, parent) } })
       const close = create('button', {type: 'button', html: '닫기', event: {click: e => $this.taskList.innerHTML = ''}})
-      task.forEach((v, idx) => ul.appendChild($this.taskChildRender(v, idx)))
+      task.forEach(v => ul.appendChild($this.taskChildRender(v)))
       $this.taskList.innerHTML = ''
       for(const ele of [title, input, ul, close]) $this.taskList.appendChild(ele)      
     }
   }
 
-  taskChildRender (v, idx) {
+  taskChildRender (v) {
+    const $this = this
     return create('li', {
       html: v.name,
       style: v.state ? 'color:#09F' : '',
-      event: {
-        click: async e => {
-          await this.model.setTask(v, idx)
-          e.target.style.color = (v.state = !v.state) ? '#09F' : ''
-        }
-      }
+      event: { click: $this.setTask(v) }
     })
   }
 }
